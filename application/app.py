b0ecf52c0cec474f2873e35e2e17cf00349d95ec
DerinSozen <67414435+DerinSozen@users.noreply.github.com>
2024-05-27 17:20:52 -0500

    latency calculations

--- application/app.py
+++ application/app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request
 import os
 import whisper
 from werkzeug.utils import secure_filename
 import wave
+from datetime import datetime
+import scipy.io
+from scipy.io import wavfile
 
+load_start_time = datetime.now()
 model = whisper.load_model("tiny.en")
+load_end_time = datetime.now()
+time_difference = (load_end_time - load_start_time).total_seconds() * 10**3
+print(f"Model loaded in {time_difference} ms")
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
@@ -23,6 +30,7 @@ def upload():
     if 'audio_data' not in request.files:
         return 'No file part', 400
     
+    response_start_time = datetime.now()
     file = request.files['audio_data']
 
     filename = secure_filename(file.filename)
@@ -32,4 +40,8 @@ def upload():
     initial_prompt = ""
     
     result = model.transcribe(filepath,initial_prompt = initial_prompt, fp16=False)
+    response_end_time = datetime.now()
+    latency = (response_end_time - response_start_time).total_seconds() * 10**3
+    print(f"latency {latency} ms")
+    
     return result["text"], 200
